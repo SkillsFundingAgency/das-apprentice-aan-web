86473887565a42c8b2837ca8c2a2dfb643b9626a
Deven Shah <devenshah1974@gmail.com>
2023-02-02 15:24:08 +0000

    CSP-559 Added onboarding base controller

---
+++ src/SFA.DAS.ApprenticeAan.Web/Controllers/Onboarding/BeforeYouStartController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.ApprenticeAan.Web.Models;
+using SFA.DAS.ApprenticeAan.Web.Services;
+
+namespace SFA.DAS.ApprenticeAan.Web.Controllers.Onboarding
+{
+    public class BeforeYouStartController : OnboardingControllerBase
+    {
+        public BeforeYouStartController(ISessionService sessionService) : base(sessionService) { }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Post()
+        {
+            SessionService.Set(new OnboardingSessionModel());
+            return Ok();
+        }
+    }
+}
---
+++ src/SFA.DAS.ApprenticeAan.Web/Controllers/Onboarding/OnboardingControllerBase.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.ApprenticeAan.Web.Models;
+using SFA.DAS.ApprenticeAan.Web.Services;
+
+namespace SFA.DAS.ApprenticeAan.Web.Controllers.Onboarding;
+
+public class OnboardingControllerBase : Controller
+{
+    protected readonly ISessionService SessionService;
+    protected OnboardingControllerBase(ISessionService sessionService)
+    {
+        SessionService = sessionService;
+    }
+
+    protected (OnboardingSessionModel?, IActionResult?) GetSessionModelWithEscapeRoute(ILogger logger)
+    {
+        var sessionModel = SessionService.Get<OnboardingSessionModel>();
+        if (sessionModel == null)
+        {
+            logger.LogInformation("Session model is invalid, redirecting to escape route.");
+            return (null, RedirectToAction("Index", "Home"));
+        }
+        return (sessionModel, null);
+    }
+}
