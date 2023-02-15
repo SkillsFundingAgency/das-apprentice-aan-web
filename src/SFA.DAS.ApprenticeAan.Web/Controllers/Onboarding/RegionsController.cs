9e116301134a50440d2bf011d01d48c3d9e888d0
Deven Shah <devenshah1974@gmail.com>
2023-02-15 11:33:18 +0000

    CSP-427 temporarily excluded RegionsController from coverage

--- src/SFA.DAS.ApprenticeAan.Web/Controllers/Onboarding/RegionsController.cs
+++ src/SFA.DAS.ApprenticeAan.Web/Controllers/Onboarding/RegionsController.cs
@@ -1,7 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.AspNetCore.Mvc;
 using SFA.DAS.ApprenticeAan.Domain.Interfaces;
 
 namespace SFA.DAS.ApprenticeAan.Web.Controllers.Onboarding;
+
+//TODO remove ExcludeFromCodeCoverage on proper implementation
+//This is initial implementation just to get api client working example
+//remove this comments once proper implementation is done
+[ExcludeFromCodeCoverage]
 
 [Route("onboarding/regions")]
 public class RegionsController : OnboardingControllerBase
