c3a958d3f61f77313faca231a344f93497f38522
Ravi Hejmadi <46343852+ravihejmadi@users.noreply.github.com>
2023-03-10 14:26:44 +0000

    CSP-517-AANOnboarding-LineManager-Revert public class OnboardingSessionModelTests.cs Sonarqube cannot recognize actualResult = model.IsValid and so is marking this as not Test Covered

--- src/SFA.DAS.ApprenticeAan.Web.UnitTests/Models/OnboardingSessionModelTests.cs
+++ src/SFA.DAS.ApprenticeAan.Web.UnitTests/Models/OnboardingSessionModelTests.cs
@@ -21,34 +21,7 @@ public class OnboardingSessionModelTests
         model.ApprenticeDetails.ApprenticeId = apprenticeId;
 
         var actualResult = model.IsValid;
-        Assert.That(actualResult, Is.EqualTo(model.ApprenticeDetails.ApprenticeId != null
-            && model.ApprenticeDetails.ApprenticeId != Guid.Empty
-            && model.HasAcceptedTermsAndConditions
-            && model.HasEmployersApproval.GetValueOrDefault()));
 
         Assert.That(actualResult, Is.EqualTo(expectedResult));
-    }
-
-    [Test]
-    public void IsValid_Validate()
-    {
-
-        var model = new OnboardingSessionModel()
-        {
-            HasAcceptedTermsAndConditions = true,
-            HasEmployersApproval = true
-        };
-        model.ApprenticeDetails.ApprenticeId = Guid.NewGuid();
-
-        Assert.Multiple(() =>
-        {
-            Assert.That(model.ApprenticeDetails, Is.Not.Null);
-            Assert.That(model.ApprenticeDetails, Is.TypeOf<ApprenticeDetailsModel>());
-            Assert.That(model.ApprenticeDetails, Is.InstanceOf<ApprenticeDetailsModel>());
-            Assert.That(model.ApprenticeDetails.ApprenticeId, Is.Not.Null);
-            Assert.That(model.ApprenticeDetails.ApprenticeId, Is.Not.EqualTo(Guid.Empty));
-            Assert.That(model.HasAcceptedTermsAndConditions, Is.True);
-            Assert.That(model.HasEmployersApproval.GetValueOrDefault(), Is.True);
-        });
     }
 }
