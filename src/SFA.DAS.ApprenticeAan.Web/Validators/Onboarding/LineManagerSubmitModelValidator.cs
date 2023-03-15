c128a9da9384130f26b33f01caa09f0490dae331
Ravi Hejmadi <46343852+ravihejmadi@users.noreply.github.com>
2023-03-15 20:40:39 +0000

    CSP-517-AANOnboarding-LineManager-Fixes for UI defects raised in Testing 1. GDS - Error side bar must cover Title 2. GDS - Top error message hyperlink must navigate focus to input control with GDS focus color 3. No input error message is too generic 4. Remove '30 minitues of inactivity' warning text

--- src/SFA.DAS.ApprenticeAan.Web/Validators/Onboarding/LineManagerSubmitModelValidator.cs
+++ src/SFA.DAS.ApprenticeAan.Web/Validators/Onboarding/LineManagerSubmitModelValidator.cs
@@ -5,7 +5,7 @@ namespace SFA.DAS.ApprenticeAan.Web.Validators.Onboarding;
 
 public class LineManagerSubmitModelValidator : AbstractValidator<LineManagerSubmitModel>
 {
-    public const string NoSelectionErrorMessage = "There is a problem. Please make a selection.";
+    public const string NoSelectionErrorMessage = "Tell us if you have approval from your line manager to join the network.";
     public const string NoApprovalErrorMessage = "You must have your manager's approval before you complete and submit your application.";
 
     public LineManagerSubmitModelValidator()
