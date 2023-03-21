6c0c2eacca8c3beacbb3daa3939d085609c0a57d
Ravi Hejmadi <46343852+ravihejmadi@users.noreply.github.com>
2023-03-21 16:50:12 +0000

    CSP-532-AANOnboarding-ProfileDataId-List of ProfileData constants (#15)

---
+++ src/SFA.DAS.ApprenticeAan.Domain/Constants/ProfileDataId.cs
@@ -0,0 +1,12 @@
+﻿namespace SFA.DAS.ApprenticeAan.Domain.Constants;
+
+public static class ProfileDataId
+{
+    public const int JobTitle = 20;
+    public const int EmployerName = 30;
+    public const int AddressLine1 = 31;
+    public const int AddressLine2 = 32;
+    public const int Town = 33;
+    public const int County = 34;
+    public const int Postcode = 35;
+}
