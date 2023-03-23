92d3460d2539fc7449e2cedba4e37974513783b3
Ravi Hejmadi <46343852+ravihejmadi@users.noreply.github.com>
2023-03-23 09:33:51 +0000

    CSP-532 Extended Onboarding model to get and set profile values
    * CSP-532 Added profile value get and set methods on session model
    
    * CSP-532 Fixed minor glitch in IsValid
    
    * CSP-532 Fixed test method name and added a missing test
    
    * CSP-532 Fixed test names
    
    ---------
    
    Co-authored-by: Deven Shah <devenshah1974@gmail.com>

--- src/SFA.DAS.ApprenticeAan.Web.UnitTests/Models/OnboardingSessionModelTests.cs
+++ src/SFA.DAS.ApprenticeAan.Web.UnitTests/Models/OnboardingSessionModelTests.cs
@@ -1,17 +1,23 @@
-﻿using SFA.DAS.ApprenticeAan.Web.Models;
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using SFA.DAS.ApprenticeAan.Web.Models;
 
 namespace SFA.DAS.ApprenticeAan.Web.UnitTests.Models;
 
 [TestFixture]
 public class OnboardingSessionModelTests
 {
-    [TestCase(null, true, true, false)]
-    [TestCase("00000000-0000-0000-0000-000000000000", true, true, false)]
-    [TestCase("bf92480f-1ef6-4552-8a5e-35d7951213f9", false, true, false)]
-    [TestCase("bf92480f-1ef6-4552-8a5e-35d7951213f9", true, false, false)]
-    [TestCase("bf92480f-1ef6-4552-8a5e-35d7951213f9", true, true, true)]
-    [TestCase("bf92480f-1ef6-4552-8a5e-35d7951213f9", true, null, false)]
-    public void IsValid_ChecksModelValidity(Guid? apprenticeId, bool hasAcceptedTAndCs, bool? hasEmployersApproval, bool expectedResult)
+    private static readonly object[] GetProfileData =
+    {
+        new object[] { new Guid("bf92480f-1ef6-4552-8a5e-35d7951213f9"), true, true, new List<ProfileModel>(), false },
+
+        new object[] { new Guid("bf92480f-1ef6-4552-8a5e-35d7951213f9"), true, true, new List<ProfileModel> { new ProfileModel() }, true },
+    };
+
+    [TestCaseSource(nameof(GetProfileData))]
+    public void IsValid_ProfileData_ChecksModelValidity(
+        Guid? apprenticeId, bool hasAcceptedTAndCs, bool? hasEmployersApproval,
+        List<ProfileModel> profileData, bool expectedResult)
     {
         var model = new OnboardingSessionModel()
         {
@@ -19,9 +25,82 @@ public class OnboardingSessionModelTests
             HasEmployersApproval = hasEmployersApproval,
         };
         model.ApprenticeDetails.ApprenticeId = apprenticeId;
+        model.ProfileData = profileData;
 
         var actualResult = model.IsValid;
 
         Assert.That(actualResult, Is.EqualTo(expectedResult));
+    }
+
+    [TestCase(null, true, true, false)]
+    [TestCase("00000000-0000-0000-0000-000000000000", true, true, false)]
+    [TestCase("bf92480f-1ef6-4552-8a5e-35d7951213f9", false, true, false)]
+    [TestCase("bf92480f-1ef6-4552-8a5e-35d7951213f9", true, false, false)]
+    [TestCase("bf92480f-1ef6-4552-8a5e-35d7951213f9", true, null, false)]
+    [TestCase("bf92480f-1ef6-4552-8a5e-35d7951213f9", true, true, true)]
+    public void IsValid_ChecksModelValidity(Guid? apprenticeId, bool hasAcceptedTAndCs, bool? hasEmployersApproval, bool expectedResult)
+    {
+        var model = new OnboardingSessionModel()
+        {
+            HasAcceptedTermsAndConditions = hasAcceptedTAndCs,
+            HasEmployersApproval = hasEmployersApproval,
+        };
+        model.ProfileData.Add(new ProfileModel());
+        model.ApprenticeDetails.ApprenticeId = apprenticeId;
+
+        var actualResult = model.IsValid;
+
+        Assert.That(actualResult, Is.EqualTo(expectedResult));
+    }
+
+    [Test]
+    [AutoData]
+    public void GetProfileValue_FoundProfileIdWithValue_ReturnsValue(OnboardingSessionModel sut, ProfileModel profileModel)
+    {
+        sut.ProfileData.Add(profileModel);
+
+        sut.GetProfileValue(profileModel.Id).Should().Be(profileModel.Value);
+    }
+
+    [Test]
+    [AutoData]
+    public void GetProfilelValue_FoundProfileIdWithNoValue_ReturnsNull(OnboardingSessionModel sut, ProfileModel profileModel)
+    {
+        profileModel.Value = null;
+        sut.ProfileData.Add(profileModel);
+
+        sut.GetProfileValue(profileModel.Id).Should().BeNull();
+    }
+
+    [Test]
+    [AutoData]
+    public void GetProfilelValue_ProfileNotFound_ThrowsInvalidOperationException(OnboardingSessionModel sut, ProfileModel profileModel)
+    {
+        profileModel.Value = null;
+
+        Action action = () => sut.GetProfileValue(profileModel.Id);
+
+        action.Should().Throw<InvalidOperationException>();
+    }
+
+    [Test]
+    [AutoData]
+    public void SetProfileValue_ProfileFound_UpdatesValue(OnboardingSessionModel sut, string value)
+    {
+        var profileModel = sut.ProfileData[0];
+
+        sut.SetProfileValue(profileModel.Id, value);
+
+        profileModel.Value.Should().Be(value);
+    }
+
+    [Test]
+    public void SetProfileValue_ProfileNotFound_ThrowsInvalidOperationException()
+    {
+        OnboardingSessionModel sut = new();
+
+        Action action = () => sut.SetProfileValue(111, "randome value");
+
+        action.Should().Throw<InvalidOperationException>();
     }
 }
--- src/SFA.DAS.ApprenticeAan.Web/Models/OnboardingSessionModel.cs
+++ src/SFA.DAS.ApprenticeAan.Web/Models/OnboardingSessionModel.cs
@@ -8,8 +8,14 @@ public class OnboardingSessionModel
     public List<ProfileModel> ProfileData { get; set; } = new List<ProfileModel>();
     public int? RegionId { get; set; }
 
-    public bool IsValid => ApprenticeDetails.ApprenticeId != null && ApprenticeDetails.ApprenticeId != Guid.Empty &&
-        HasAcceptedTermsAndConditions && HasEmployersApproval.GetValueOrDefault();
+    public bool IsValid =>
+        ApprenticeDetails.ApprenticeId.GetValueOrDefault() != Guid.Empty &&
+        HasAcceptedTermsAndConditions && HasEmployersApproval.GetValueOrDefault() &&
+        ProfileData.Count > 0;
+
+    public string? GetProfileValue(int id) => ProfileData.Single(p => p.Id == id)?.Value;
+
+    public void SetProfileValue(int id, string value) => ProfileData.Single(p => p.Id == id).Value = value;
 }
 
 public class ApprenticeDetailsModel
