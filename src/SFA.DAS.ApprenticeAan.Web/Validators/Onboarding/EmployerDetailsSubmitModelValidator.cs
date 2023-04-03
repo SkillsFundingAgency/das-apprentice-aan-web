c6b94bd99ed5df0fc54bd568461cc8e78831d5b6
Ravi Hejmadi <46343852+ravihejmadi@users.noreply.github.com>
2023-04-03 14:59:26 +0100

    CSP-532-AANOnboarding-OnboardingSessionModel - Remove the word 'Please' in validation messages as per GDS

--- src/SFA.DAS.ApprenticeAan.Web/Validators/Onboarding/EmployerDetailsSubmitModelValidator.cs
+++ src/SFA.DAS.ApprenticeAan.Web/Validators/Onboarding/EmployerDetailsSubmitModelValidator.cs
@@ -7,11 +7,11 @@ namespace SFA.DAS.ApprenticeAan.Web.Validators.Onboarding;
 
 public class EmployerDetailsSubmitModelValidator : AbstractValidator<EmployerDetailsSubmitModel>
 {
-    public const string EmployerNameEmptyMessage = "Please enter an Employer Name";
+    public const string EmployerNameEmptyMessage = "Enter an Employer Name";
     public const string EmployerNameMaxLengthMessage = "Your Employer Name must be up to 200 characters";
     public const string EmployerNameHasExcludedCharacter = "Your Employer Name must not include any special characters: @, #, $, ^, =, +, \\, /, <, >, %";
 
-    public const string AddressLine1EmptyMessage = "Please enter the Building and Street";
+    public const string AddressLine1EmptyMessage = "Enter the Building and Street";
     public const string AddressLine1MaxLengthMessage = "Your Employer Address Line 1 must be up to 200 characters";
     public const string AddressLine1HasExcludedCharacter = "Your Employer Address Line 1 must not include any special characters: @, #, $, ^, =, +, \\, /, <, >, %";
 
@@ -21,12 +21,12 @@ public class EmployerDetailsSubmitModelValidator : AbstractValidator<EmployerDet
     public const string AddressLine3MaxLengthMessage = "Your Employer Address Line 3 must be up to 200 characters";
     public const string AddressLine3HasExcludedCharacter = "Your Employer Address Line 3 must not include any special characters: @, #, $, ^, =, +, \\, /, <, >, %";
 
-    public const string TownOrCityEmptyMessage = "Please enter a Town or city";
+    public const string TownOrCityEmptyMessage = "Enter a Town or city";
     public const string TownOrCityMaxLengthMessage = "Your Employer Town or city must be up to 200 characters";
     public const string TownOrCityHasExcludedCharacter = "Your Employer Town or city must not include any special characters: @, #, $, ^, =, +, \\, /, <, >, %";
 
-    public const string PostcodeEmptyMessage = "Please enter a Postcode";
-    public const string PostcodeInvalidMessage = "Please enter a real Postcode";
+    public const string PostcodeEmptyMessage = "Enter a Postcode";
+    public const string PostcodeInvalidMessage = "Enter a real Postcode";
 
     public EmployerDetailsSubmitModelValidator()
     {
