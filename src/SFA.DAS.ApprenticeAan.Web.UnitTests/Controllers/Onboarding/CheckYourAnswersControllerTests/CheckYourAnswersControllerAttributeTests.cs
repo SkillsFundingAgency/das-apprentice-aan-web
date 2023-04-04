7c43c6b818633457cee5f8ba81cbb97e989daebe
Ravi Hejmadi <46343852+ravihejmadi@users.noreply.github.com>
2023-04-04 15:52:15 +0100

    CSP-666-CommonBitsForCheckYourAnswers - Add coverage

---
+++ src/SFA.DAS.ApprenticeAan.Web.UnitTests/Controllers/Onboarding/CheckYourAnswersControllerTests/CheckYourAnswersControllerAttributeTests.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.ApprenticeAan.Domain.Constants;
+using SFA.DAS.ApprenticeAan.Web.Controllers;
+using SFA.DAS.ApprenticeAan.Web.Controllers.Onboarding;
+using SFA.DAS.ApprenticeAan.Web.Filters;
+using SFA.DAS.ApprenticeAan.Web.Infrastructure;
+using SFA.DAS.ApprenticeAan.Web.Models;
+using SFA.DAS.ApprenticeAan.Web.Models.Onboarding;
+using System.Security.Policy;
+
+namespace SFA.DAS.ApprenticeAan.Web.UnitTests.Controllers.Onboarding.CheckYourAnswersControllerTests;
+
+[TestFixture]
+public class CheckYourAnswersControllerAttributeTests
+{
+    [Test]
+    public void Controller_HasCorrectRouteAttribute()
+    {
+        typeof(CheckYourAnswersController).Should().BeDecoratedWith<RouteAttribute>();
+        typeof(CheckYourAnswersController).Should().BeDecoratedWith<RouteAttribute>().Subject.Template.Should().Be("onboarding/check-your-answers");
+        typeof(CheckYourAnswersController).Should().BeDecoratedWith<RouteAttribute>().Subject.Name.Should().Be(RouteNames.Onboarding.CheckYourAnswers);
+    }
+
+    [Test]
+    public void Controller_HasRequiredSessionModelAttribute()
+    {
+        typeof(CheckYourAnswersController).Should().BeDecoratedWith<RequiredSessionModelAttribute>();
+    }
+}
