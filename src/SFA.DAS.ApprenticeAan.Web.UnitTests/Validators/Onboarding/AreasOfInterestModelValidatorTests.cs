71d8c782459f87b901953aa401736e30d2f5e52b
Yuvraj Patil <yuvraj.patil@digital.education.gov.uk>
2023-04-04 18:19:13 +0100

    CSP-491-Added code coverage for validator

---
+++ src/SFA.DAS.ApprenticeAan.Web.UnitTests/Validators/Onboarding/AreasOfInterestModelValidatorTests.cs
@@ -0,0 +1,70 @@
+﻿using FluentValidation.TestHelper;
+using SFA.DAS.ApprenticeAan.Web.Models;
+using SFA.DAS.ApprenticeAan.Web.Models.Onboarding;
+using SFA.DAS.ApprenticeAan.Web.Validators.Onboarding;
+
+namespace SFA.DAS.ApprenticeAan.Web.UnitTests.Validators.Onboarding;
+
+[TestFixture]
+public class AreasOfInterestModelValidatorTests
+{
+    [Test]
+    public void NoSelection_Validation_Error()
+    {
+        var model = new AreasOfInterestSubmitModel
+        {
+            Events = new List<SelectProfileModel> { new SelectProfileModel { Id = 1, IsSelected = false } },
+            Promotions = new List<SelectProfileModel> { new SelectProfileModel { Id = 2, IsSelected = false } }
+        };
+
+        var sut = new AreasOfInterestSubmitModelValidator();
+        var result = sut.TestValidate(model);
+
+        result.ShouldHaveValidationErrorFor(c => c.AreasOfInterest).WithErrorMessage(AreasOfInterestSubmitModelValidator.NoSelectionErrorMessage);
+    }
+
+    [Test]
+    public void EventsSelection_Validation_NoError()
+    {
+        var model = new AreasOfInterestSubmitModel
+        {
+            Events = new List<SelectProfileModel> { new SelectProfileModel { Id = 1, IsSelected = true } },
+            Promotions = new List<SelectProfileModel> { new SelectProfileModel { Id = 2, IsSelected = false } }
+        };
+
+        var sut = new AreasOfInterestSubmitModelValidator();
+        var result = sut.TestValidate(model);
+
+        result.ShouldNotHaveValidationErrorFor(c => c.AreasOfInterest);
+    }
+
+    [Test]
+    public void PromotionsSelection_Validation_NoError()
+    {
+        var model = new AreasOfInterestSubmitModel
+        {
+            Events = new List<SelectProfileModel> { new SelectProfileModel { Id = 1, IsSelected = false } },
+            Promotions = new List<SelectProfileModel> { new SelectProfileModel { Id = 2, IsSelected = true } }
+        };
+
+        var sut = new AreasOfInterestSubmitModelValidator();
+        var result = sut.TestValidate(model);
+
+        result.ShouldNotHaveValidationErrorFor(c => c.AreasOfInterest);
+    }
+
+    [Test]
+    public void EventsAndPromotionsSelection_Validation_NoError()
+    {
+        var model = new AreasOfInterestSubmitModel
+        {
+            Events = new List<SelectProfileModel> { new SelectProfileModel { Id = 1, IsSelected = true } },
+            Promotions = new List<SelectProfileModel> { new SelectProfileModel { Id = 2, IsSelected = true } }
+        };
+
+        var sut = new AreasOfInterestSubmitModelValidator();
+        var result = sut.TestValidate(model);
+
+        result.ShouldNotHaveValidationErrorFor(c => c.AreasOfInterest);
+    }
+}
--- src/SFA.DAS.ApprenticeAan.Web/Validators/Onboarding/AreasOfInterestSubmitModelValidator.cs
+++ src/SFA.DAS.ApprenticeAan.Web/Validators/Onboarding/AreasOfInterestSubmitModelValidator.cs
@@ -3,11 +3,11 @@ using SFA.DAS.ApprenticeAan.Web.Models.Onboarding;
 
 namespace SFA.DAS.ApprenticeAan.Web.Validators.Onboarding;
 
-public class AreasOfInterestModelValidator : AbstractValidator<AreasOfInterestSubmitModel>
+public class AreasOfInterestSubmitModelValidator : AbstractValidator<AreasOfInterestSubmitModel>
 {
     public const string NoSelectionErrorMessage = "Select an area of interest";
 
-    public AreasOfInterestModelValidator()
+    public AreasOfInterestSubmitModelValidator()
     {
         RuleLevelCascadeMode = CascadeMode.Stop;
         RuleFor(x => x.AreasOfInterest).Must(c => c.Any(p => p.IsSelected)).WithMessage(NoSelectionErrorMessage);
