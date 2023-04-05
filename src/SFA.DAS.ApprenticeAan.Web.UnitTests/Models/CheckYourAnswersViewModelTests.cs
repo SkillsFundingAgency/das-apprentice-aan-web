560de8202b5ae151e8a32ec92b2493a84018c608
Ravi Hejmadi <46343852+ravihejmadi@users.noreply.github.com>
2023-04-05 10:39:18 +0100

    CSP-666-CommonBitsForCheckYourAnswers - Add Ctor unit test

---
+++ src/SFA.DAS.ApprenticeAan.Web.UnitTests/Models/CheckYourAnswersViewModelTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using SFA.DAS.ApprenticeAan.Domain.Constants;
+using SFA.DAS.ApprenticeAan.Web.Infrastructure;
+using SFA.DAS.ApprenticeAan.Web.Models;
+using SFA.DAS.ApprenticeAan.Web.Models.Onboarding;
+using SFA.DAS.ApprenticeAan.Web.UnitTests.TestHelpers;
+
+namespace SFA.DAS.ApprenticeAan.Web.UnitTests.Models;
+
+[TestFixture]
+public class CheckYourAnswersViewModelTests
+{
+    [Test]
+    public void Ctor_SetsJobTitleChangeLink()
+    {
+        var expectedJobTitle = "Some Title";
+        Mock<IUrlHelper> mockUrlHelper = new();
+        mockUrlHelper.AddUrlForRoute(RouteNames.Onboarding.CurrentJobTitle);
+
+        OnboardingSessionModel sessionModel = new();
+        sessionModel.ProfileData.Add(new ProfileModel { Id = ProfileDataId.JobTitle, Value = expectedJobTitle });
+
+        var checkYourAnswersViewModel = new CheckYourAnswersViewModel(mockUrlHelper.Object, sessionModel);
+
+        Assert.That(checkYourAnswersViewModel.JobTitleChangeLink.Equals(TestConstants.DefaultUrl));
+    }
+
+    [Test]
+    public void Ctor_SetsJobTitle()
+    {
+        var expectedJobTitle = "Some Title";
+        Mock<IUrlHelper> mockUrlHelper = new();
+        OnboardingSessionModel sessionModel = new();
+        sessionModel.ProfileData.Add(new ProfileModel { Id = ProfileDataId.JobTitle, Value = expectedJobTitle });
+
+        var checkYourAnswersViewModel = new CheckYourAnswersViewModel(mockUrlHelper.Object, sessionModel);
+
+        Assert.That(checkYourAnswersViewModel.JobTitle.Equals(expectedJobTitle));
+    }
+}
