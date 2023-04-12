d2709f3bd4788d715f71908e10828d0d2aab244a
Deven Shah <devenshah1974@gmail.com>
2023-04-12 10:59:40 +0100

    CSP-487 Added validation for postcode to indicate address requirement

--- src/SFA.DAS.ApprenticeAan.Web/Validators/Onboarding/EmployerSearchSubmitModelValidator.cs
+++ src/SFA.DAS.ApprenticeAan.Web/Validators/Onboarding/EmployerSearchSubmitModelValidator.cs
@@ -7,10 +7,16 @@ public class EmployerSearchSubmitModelValidator : AbstractValidator<EmployerSear
 {
     private const string MissingSearchTermErrorMessage = "Enter an employer name or postcode";
 
+    private const string MissingAddressErrorMessage = "Enter an employer name or postcode";
+
     public EmployerSearchSubmitModelValidator()
     {
         RuleFor(m => m.SearchTerm)
             .NotEmpty()
             .WithMessage(MissingSearchTermErrorMessage);
+
+        RuleFor(e => e.Postcode)
+            .NotEmpty()
+            .WithMessage(MissingAddressErrorMessage);
     }
 }
