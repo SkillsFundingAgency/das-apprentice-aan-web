daf07b3b596dae8653f0dcb8480734044c760b61
Deven Shah <devenshah1974@gmail.com>
2023-04-13 09:19:44 +0100

    CSP-497 Dynamically change header on employer details page

---
+++ src/SFA.DAS.ApprenticeAan.Web.UnitTests/Models/Onboarding/EmployerDetailsViewModelTests.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using SFA.DAS.ApprenticeAan.Web.Models.Onboarding;
+
+namespace SFA.DAS.ApprenticeAan.Web.UnitTests.Models.Onboarding;
+
+public class EmployerDetailsViewModelTests
+{
+    [TestCase("CV1 1EF", EmployerDetailsViewModel.CheckDetailsHeader)]
+    [TestCase(" ", EmployerDetailsViewModel.AddDetailsHeader)]
+    [TestCase("", EmployerDetailsViewModel.AddDetailsHeader)]
+    [TestCase(null, EmployerDetailsViewModel.AddDetailsHeader)]
+    public void HasAddress_IsSetBasedOnPostcode(string? postcode, string expectedValue)
+    {
+        EmployerDetailsViewModel sut = new() { Postcode = postcode };
+
+        sut.PageHeader.Should().Be(expectedValue);
+    }
+}
