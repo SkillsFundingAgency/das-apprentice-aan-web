0485f9eb71795d778e919f9263e568d0f8d9dc48
Deven Shah <devenshah1974@gmail.com>
2023-04-14 16:33:41 +0100

    CSP-744 Simplified terms and conditions

---
+++ src/SFA.DAS.ApprenticeAan.Web/Infrastructure/TempDataKeys.cs
@@ -0,0 +1,6 @@
+﻿namespace SFA.DAS.ApprenticeAan.Web.Infrastructure;
+
+public static class TempDataKeys
+{
+    public const string HasSeenTermsAndConditions = nameof(HasSeenTermsAndConditions);
+}
