65f93bf8e3ea40dd699477fc410ee9a7a0ac28e2
Deven Shah <devenshah1974@gmail.com>
2023-04-17 09:46:29 +0100

    CSP-744 Excluded TempDataKeys from coverage

--- src/SFA.DAS.ApprenticeAan.Web/Infrastructure/TempDataKeys.cs
+++ src/SFA.DAS.ApprenticeAan.Web/Infrastructure/TempDataKeys.cs
@@ -1,5 +1,8 @@
-﻿namespace SFA.DAS.ApprenticeAan.Web.Infrastructure;
+﻿using System.Diagnostics.CodeAnalysis;
 
+namespace SFA.DAS.ApprenticeAan.Web.Infrastructure;
+
+[ExcludeFromCodeCoverage]
 public static class TempDataKeys
 {
     public const string HasSeenTermsAndConditions = nameof(HasSeenTermsAndConditions);
