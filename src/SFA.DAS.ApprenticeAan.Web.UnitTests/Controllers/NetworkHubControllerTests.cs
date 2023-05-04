25ff9619442d8c6a0b0ebfddec998290fa45f1a9
Deven Shah <devenshah1974@gmail.com>
2023-05-04 16:02:16 +0100

    CSP-600 Implements network hub page

---
+++ src/SFA.DAS.ApprenticeAan.Web.UnitTests/Controllers/NetworkHubControllerTests.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.ApprenticeAan.Web.Controllers;
+using SFA.DAS.ApprenticeAan.Web.Models;
+
+namespace SFA.DAS.ApprenticeAan.Web.UnitTests.Controllers;
+
+public class NetworkHubControllerTests
+{
+    [Test]
+    public void Index_ReturnsView()
+    {
+        NetworkHubController sut = new();
+
+        var result = sut.Index();
+
+        result.As<ViewResult>().Model.As<NetworkHubViewModel>().Should().NotBeNull();
+    }
+}
---
+++ src/SFA.DAS.ApprenticeAan.Web/Controllers/NetworkHubController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.ApprenticeAan.Web.Infrastructure;
+using SFA.DAS.ApprenticeAan.Web.Models;
+
+namespace SFA.DAS.ApprenticeAan.Web.Controllers;
+
+[Authorize]
+[Route("network-hub", Name = RouteNames.NetworkHub)]
+public class NetworkHubController : Controller
+{
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View(new NetworkHubViewModel());
+    }
+}
