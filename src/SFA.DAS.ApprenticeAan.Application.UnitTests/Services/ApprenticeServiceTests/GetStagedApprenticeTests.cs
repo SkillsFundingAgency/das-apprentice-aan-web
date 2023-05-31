18e5c388b9e1c80b6e1f67e01fb6ae6e7cc8e02c
Deven Shah <devenshah1974@gmail.com>
2023-05-31 17:15:24 +0100

    CSP-792 Added GetStagedApprentice service method

---
+++ src/SFA.DAS.ApprenticeAan.Application.UnitTests/Services/ApprenticeServiceTests/GetStagedApprenticeTests.cs
@@ -0,0 +1,73 @@
+﻿using System.Net;
+using AutoFixture.NUnit3;
+using FluentAssertions;
+using Moq;
+using RestEase;
+using SFA.DAS.ApprenticeAan.Application.Services;
+using SFA.DAS.ApprenticeAan.Domain.Interfaces;
+using SFA.DAS.ApprenticeAan.Domain.OuterApi.Responses;
+using SFA.DAS.Testing.AutoFixture;
+
+namespace SFA.DAS.ApprenticeAan.Application.UnitTests.Services.ApprenticeServiceTests;
+
+public class GetStagedApprenticeTests
+{
+    [Test, MoqAutoData]
+    public async Task GetStagedApprentice_InvokesApiClient(
+        [Frozen] Mock<IOuterApiClient> clientMock,
+        ApprenticeService sut,
+        string lastName, DateTime dateOfBirth, string email,
+        Response<StagedApprentice?> stagedApprenticeResponse,
+        CancellationToken cancellationToken)
+    {
+        stagedApprenticeResponse.ResponseMessage.StatusCode = HttpStatusCode.OK;
+        clientMock.Setup(c => c.GetStagedApprentice(lastName, dateOfBirth, email, cancellationToken)).ReturnsAsync(stagedApprenticeResponse);
+
+        await sut.GetStagedApprentice(lastName, dateOfBirth, email, cancellationToken);
+
+        clientMock.Verify(c => c.GetStagedApprentice(lastName, dateOfBirth, email, cancellationToken));
+    }
+
+    [Test, MoqAutoData]
+    public async Task GetStagedApprentice_FoundApprentice_ReturnsApprentice(
+        [Frozen] Mock<IOuterApiClient> clientMock,
+        ApprenticeService sut,
+        string lastName, DateTime dateOfBirth, string email,
+        Response<StagedApprentice?> stagedApprenticeResponse,
+        CancellationToken cancellationToken)
+    {
+        stagedApprenticeResponse.ResponseMessage.StatusCode = HttpStatusCode.OK;
+        clientMock.Setup(c => c.GetStagedApprentice(lastName, dateOfBirth, email, cancellationToken)).ReturnsAsync(stagedApprenticeResponse);
+
+        var result = await sut.GetStagedApprentice(lastName, dateOfBirth, email, cancellationToken);
+
+        result.Should().NotBeNull();
+    }
+
+    [Test, MoqAutoData]
+    public async Task GetStagedApprentice_NotFound_ReturnsNull(
+        [Frozen] Mock<IOuterApiClient> clientMock,
+        ApprenticeService sut,
+        string lastName, DateTime dateOfBirth, string email,
+        Response<StagedApprentice?> stagedApprenticeResponse,
+        CancellationToken cancellationToken)
+    {
+        stagedApprenticeResponse.ResponseMessage.StatusCode = HttpStatusCode.NotFound;
+        clientMock.Setup(c => c.GetStagedApprentice(lastName, dateOfBirth, email, cancellationToken)).ReturnsAsync(stagedApprenticeResponse);
+
+        var result = await sut.GetStagedApprentice(lastName, dateOfBirth, email, cancellationToken);
+
+        result.Should().BeNull();
+    }
+
+    [Test, MoqAutoData]
+    public async Task GetStagedApprentice_OuterApiUnsuccessfulResponse_ThrowsInvalidOperationException(
+        ApprenticeService sut,
+        string lastName, DateTime dateOfBirth, string email,
+        CancellationToken cancellationToken)
+    {
+        Func<Task> action = () => sut.GetStagedApprentice(lastName, dateOfBirth, email, cancellationToken);
+
+        await action.Should().ThrowAsync<InvalidOperationException>();
+    }
+}
--- src/SFA.DAS.ApprenticeAan.Domain/Interfaces/IApprenticeService.cs
+++ src/SFA.DAS.ApprenticeAan.Domain/Interfaces/IApprenticeService.cs
@@ -5,4 +5,5 @@ namespace SFA.DAS.ApprenticeAan.Domain.Interfaces;
 public interface IApprenticeService
 {
     Task<Apprentice?> GetApprentice(Guid apprenticeId);
+    Task<StagedApprentice?> GetStagedApprentice(string lastName, DateTime dateOfBirth, string email, CancellationToken cancellationToken = new());
 }
