0d2bec47fa1ef3bd064f4107bc744e61cfb27509
Deven Shah <devenshah1974@gmail.com>
2023-06-08 08:13:21 +0100

    CSP-836 Enabled redis distributed cache

---
+++ src/SFA.DAS.ApprenticeAan.Web/AppStart/LoadConfigurationExtension.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using SFA.DAS.ApprenticeAan.Web.Configuration;
+using SFA.DAS.Configuration.AzureTableStorage;
+
+namespace SFA.DAS.ApprenticeAan.Web.AppStart;
+
+[ExcludeFromCodeCoverage]
+public static class LoadConfigurationExtension
+{
+    public static IConfigurationRoot LoadConfiguration(this IConfiguration config, IServiceCollection services)
+    {
+        var configBuilder = new ConfigurationBuilder()
+            .AddConfiguration(config)
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddEnvironmentVariables();
+
+
+        if (!config["EnvironmentName"].Equals("DEV", StringComparison.CurrentCultureIgnoreCase))
+        {
+            configBuilder.AddAzureTableStorage(options =>
+            {
+                options.ConfigurationKeys = config["ConfigNames"].Split(",");
+                options.StorageConnectionString = config["ConfigurationStorageConnectionString"];
+                options.EnvironmentName = config["EnvironmentName"];
+                options.PreFixConfigurationKeys = false;
+            });
+        }
+
+        var configuration = configBuilder.Build();
+
+        var appConfig = configuration.Get<ApplicationConfiguration>();
+        services.AddSingleton(appConfig);
+
+        return configuration;
+    }
+}
