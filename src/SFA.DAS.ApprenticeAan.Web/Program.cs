d7af521e9bbff825177d0697fa397b12b746607d
Deven Shah <devenshah1974@gmail.com>
2023-06-08 08:30:53 +0100

    CSP-836 Fix minor issue

--- src/SFA.DAS.ApprenticeAan.Web/Program.cs
+++ src/SFA.DAS.ApprenticeAan.Web/Program.cs
@@ -55,7 +55,7 @@ builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
 }
