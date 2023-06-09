aab8c0d34b9d098fad5f673b599ddb432e4c2ad3
Deven Shah <devenshah1974@gmail.com>
2023-06-09 10:43:46 +0100

    CSP-601 Added events hub link to network hub

--- src/SFA.DAS.ApprenticeAan.Web.UnitTests/Controllers/NetworkHubControllerTests.cs
+++ src/SFA.DAS.ApprenticeAan.Web.UnitTests/Controllers/NetworkHubControllerTests.cs
@@ -1,19 +1,36 @@
 ﻿using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
 using SFA.DAS.ApprenticeAan.Web.Controllers;
+using SFA.DAS.ApprenticeAan.Web.Infrastructure;
 using SFA.DAS.ApprenticeAan.Web.Models;
+using SFA.DAS.ApprenticeAan.Web.UnitTests.TestHelpers;
 
 namespace SFA.DAS.ApprenticeAan.Web.UnitTests.Controllers;
 
 public class NetworkHubControllerTests
 {
-    [Test]
-    public void Index_ReturnsView()
+    private IActionResult _result = null!;
+    static readonly string EventsHubUrl = Guid.NewGuid().ToString();
+
+    [SetUp]
+    public void WhenGettingNetworkHub()
     {
         NetworkHubController sut = new();
+        sut.AddUrlHelperMock().AddUrlForRoute(RouteNames.EventsHub, EventsHubUrl);
 
-        var result = sut.Index();
+        _result = sut.Index();
+    }
 
-        result.As<ViewResult>().Model.As<NetworkHubViewModel>().Should().NotBeNull();
+    [Test]
+    public void ThenReturnsView()
+    {
+        _result.As<ViewResult>().Should().NotBeNull();
+    }
+
+    [Test]
+    public void ThenSetsEventsHubUrlInViewModel()
+    {
+        var model = _result.As<ViewResult>().Model.As<NetworkHubViewModel>();
+        model.EventsHubUrl.Should().Be(EventsHubUrl);
     }
 }
