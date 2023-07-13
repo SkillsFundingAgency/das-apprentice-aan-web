f747ac18b7426599d743705d092c40089878bdf9
MarkFCain <mark.cain@digital.education.gov.uk>
2023-07-13 11:47:44 +0100

    CSP-783 removed commented out code from PaginationViewModel

--- src/SFA.DAS.ApprenticeAan.Web/Models/PaginationViewModel.cs
+++ src/SFA.DAS.ApprenticeAan.Web/Models/PaginationViewModel.cs
@@ -21,19 +21,6 @@ public class PaginationViewModel
 
         if (totalPages == 0) return;
 
-        // var startPage = currentPage < 4 ? 1 : currentPage - 2;
-        // if (totalPages <= 6)
-        // {
-        //     startPage = 1;
-        // }
-        // else
-        // {
-        //     /// This deals with when the current page is the last page, second to last page, or third to last page
-        //     if (currentPage >= totalPages - 2)
-        //     {
-        //         startPage = currentPage - 5 + (totalPages - currentPage);
-        //     }
-        //}
         var startPage =
             (totalPages < 7 || currentPage < 4)
                 ? 1
