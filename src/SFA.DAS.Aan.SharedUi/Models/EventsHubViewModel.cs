ae4319daaeae0e9b04123d4947e4e8a52f045d08
Manish Agarwal <Manish.Agarwal@education.gov.uk>
2023-07-19 13:03:17 +0100

    CSP-882 Fixed warning for _urlHelper

--- src/SFA.DAS.Aan.SharedUi/Models/EventsHubViewModel.cs
+++ src/SFA.DAS.Aan.SharedUi/Models/EventsHubViewModel.cs
@@ -5,19 +5,15 @@ namespace SFA.DAS.Aan.SharedUi.Models;
 
 public class EventsHubViewModel
 {
-    private readonly IUrlHelper _urlHelper;
     public string AllNetworksUrl { get; init; }
 
     public CalendarViewModel Calendar { get; init; }
 
     public EventsHubViewModel(DateOnly firstDayOfTheMonth, IUrlHelper urlHelper, List<Appointment> appointments)
     {
-        _urlHelper = urlHelper;
         AllNetworksUrl = urlHelper.RouteUrl(SharedRouteNames.NetworkEvents)!;
         Calendar = new(firstDayOfTheMonth, DateOnly.FromDateTime(DateTime.Today), appointments);
-        Calendar.PreviousMonthLink = _urlHelper.RouteUrl(SharedRouteNames.EventsHub, new { firstDayOfTheMonth.AddMonths(-1).Month, firstDayOfTheMonth.AddMonths(-1).Year })!;
-        Calendar.NextMonthLink = _urlHelper.RouteUrl(SharedRouteNames.EventsHub, new { firstDayOfTheMonth.AddMonths(1).Month, firstDayOfTheMonth.AddMonths(1).Year })!;
+        Calendar.PreviousMonthLink = urlHelper.RouteUrl(SharedRouteNames.EventsHub, new { firstDayOfTheMonth.AddMonths(-1).Month, firstDayOfTheMonth.AddMonths(-1).Year })!;
+        Calendar.NextMonthLink = urlHelper.RouteUrl(SharedRouteNames.EventsHub, new { firstDayOfTheMonth.AddMonths(1).Month, firstDayOfTheMonth.AddMonths(1).Year })!;
     }
-
-
 }
