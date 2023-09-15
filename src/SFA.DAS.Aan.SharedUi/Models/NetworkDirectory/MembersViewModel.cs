8e1975c7bee91899dc29a2907e868774305a8638
ManishAgarwalDfE <139247114+ManishAgarwalDfE@users.noreply.github.com>
2023-09-15 13:37:04 +0100

    CSP-876 and CSP-887 PR Comments fixed

--- src/SFA.DAS.Aan.SharedUi/Models/NetworkDirectory/MembersViewModel.cs
+++ src/SFA.DAS.Aan.SharedUi/Models/NetworkDirectory/MembersViewModel.cs
@@ -5,11 +5,11 @@ namespace SFA.DAS.Aan.SharedUi.Models.NetworkDirectory;
 public class MembersViewModel
 {
     public Guid MemberId { get; set; }
-    public string? FullName { get; set; } = null!;
-    public int? RegionId { get; set; } = null!;
-    public string? RegionName { get; set; } = null!;
+    public string? FullName { get; set; }
+    public int? RegionId { get; set; }
+    public string? RegionName { get; set; }
     public Role UserRole { get; set; }
-    public bool? IsRegionalChair { get; set; } = null!;
+    public bool? IsRegionalChair { get; set; }
     public DateTime JoinedDate { get; set; }
 
     public static implicit operator MembersViewModel(MemberSummary source)
