7fbe71dfd15636c05ef330693a6571a2a9c37650
ManishAgarwalDfE <139247114+ManishAgarwalDfE@users.noreply.github.com>
2023-10-13 14:54:21 +0100

    CSP-719: Fixed the merge issue

--- src/SFA.DAS.Aan.SharedUi/Models/AmbassadorProfile/MemberUserType.cs
+++ src/SFA.DAS.Aan.SharedUi/Models/AmbassadorProfile/MemberUserType.cs
@@ -1,3 +1,4 @@
 using System.ComponentModel;
 
+namespace SFA.DAS.Aan.SharedUi.Models.AmbassadorProfile;
 public enum MemberUserType { [Description("Apprentice")] Apprentice, [Description("Employer")] Employer, [Description("Regional Chair")] RegionalChair }
