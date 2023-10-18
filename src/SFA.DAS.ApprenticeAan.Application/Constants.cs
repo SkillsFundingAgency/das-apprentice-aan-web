f54d6d43b8403b3ee506e114f750bf4b441475b6
Deven Shah <devenshah1974@gmail.com>
2023-10-18 13:34:22 +0100

    CSP-975 Implements notification endpoint (#79)
    * CSP-975 Implements notification endpoint
    
    * CSP-975 Fixing the route name post merge
    
    * CSP-975 Changed route path of notifications to use links

--- src/SFA.DAS.ApprenticeAan.Application/Constants.cs
+++ src/SFA.DAS.ApprenticeAan.Application/Constants.cs
@@ -7,5 +7,19 @@
             public const string ExcludedCharactersRegex = @"^[^@#$^=+\\\/<>%]*$";
             public const string PostcodeRegex = @"^[a-zA-z]{1,2}\d[a-zA-z\d]?\s*\d[a-zA-Z]{2}$";
         }
+
+        public static class NotificationTemplateNames
+        {
+            public const string AANApprenticeOnboarding = nameof(AANApprenticeOnboarding);
+            public const string AANApprenticeEventSignup = nameof(AANApprenticeEventSignup);
+            public const string AANApprenticeEventCancel = nameof(AANApprenticeEventCancel);
+            public const string AANApprenticeWithdrawal = nameof(AANApprenticeWithdrawal);
+            public const string AANAdminEventUpdate = nameof(AANAdminEventUpdate);
+            public const string AANAdminEventCancel = nameof(AANAdminEventCancel);
+            public const string AANIndustryAdvice = nameof(AANIndustryAdvice);
+            public const string AANAskForHelp = nameof(AANAskForHelp);
+            public const string AANRequestCaseStudy = nameof(AANRequestCaseStudy);
+            public const string AANGetInTouch = nameof(AANGetInTouch);
+        }
     }
 }
---
+++ src/SFA.DAS.ApprenticeAan.Web/Controllers/NotificationsController.cs
@@ -0,0 +1,55 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.Aan.SharedUi.Infrastructure;
+using SFA.DAS.ApprenticeAan.Domain.Interfaces;
+using SFA.DAS.ApprenticeAan.Web.Extensions;
+using SFA.DAS.ApprenticeAan.Web.Infrastructure;
+using static SFA.DAS.ApprenticeAan.Application.Constants;
+
+namespace SFA.DAS.ApprenticeAan.Web.Controllers;
+
+[Authorize]
+public class NotificationsController : Controller
+{
+    private readonly IOuterApiClient _outerApiClient;
+
+    public NotificationsController(IOuterApiClient outerApiClient)
+    {
+        _outerApiClient = outerApiClient;
+    }
+
+    [Route("links/{id}")]
+    public async Task<IActionResult> Index(Guid id, CancellationToken cancellationToken)
+    {
+        var response = await _outerApiClient.GetNotification(User.GetAanMemberId(), id, cancellationToken);
+
+        if (response.ResponseMessage.StatusCode != HttpStatusCode.OK) return RedirectToRoute(RouteNames.Home);
+
+        var notification = response.GetContent()!;
+
+        (string RouteName, object? RouteValues) route = notification.TemplateName switch
+        {
+            NotificationTemplateNames.AANApprenticeOnboarding
+                => (RouteNames.NetworkHub, null),
+
+            NotificationTemplateNames.AANApprenticeEventSignup
+            or NotificationTemplateNames.AANAdminEventUpdate
+            or NotificationTemplateNames.AANAdminEventCancel
+                => (SharedRouteNames.NetworkEventDetails, new { id = notification.ReferenceId }),
+
+            NotificationTemplateNames.AANApprenticeEventCancel
+                => (SharedRouteNames.NetworkEvents, null),
+
+            NotificationTemplateNames.AANIndustryAdvice
+            or NotificationTemplateNames.AANAskForHelp
+            or NotificationTemplateNames.AANRequestCaseStudy
+            or NotificationTemplateNames.AANGetInTouch
+                => (SharedRouteNames.MemberProfile, new { id = notification.ReferenceId }),
+
+            _ => (RouteNames.Home, null)
+        };
+
+        return RedirectToRoute(route.RouteName, route.RouteValues);
+    }
+}
