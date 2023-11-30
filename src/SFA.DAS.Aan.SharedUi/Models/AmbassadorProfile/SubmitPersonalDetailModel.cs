eea23382bb2fb5fb581d8acda04a6a6da44d0f4a
ManishAgarwalDfE <139247114+ManishAgarwalDfE@users.noreply.github.com>
2023-11-30 09:29:47 +0000

    CSP-713 Fixed Validation summary,validation message font weight and link color

--- src/SFA.DAS.Aan.SharedUi/Models/AmbassadorProfile/SubmitPersonalDetailModel.cs
+++ src/SFA.DAS.Aan.SharedUi/Models/AmbassadorProfile/SubmitPersonalDetailModel.cs
@@ -5,10 +5,10 @@ namespace SFA.DAS.Aan.SharedUi.Models;
 public class SubmitPersonalDetailModel
 {
     public int RegionId { get; set; }
-    public string? Biography { get; set; }
-    public bool ShowBiography { get; set; }
     public string? JobTitle { get; set; }
     public bool ShowJobTitle { get; set; }
+    public string? Biography { get; set; }
+    public bool ShowBiography { get; set; }
     public string OrganisationName { get; set; } = null!;
     public MemberUserType UserType { get; set; }
 }
