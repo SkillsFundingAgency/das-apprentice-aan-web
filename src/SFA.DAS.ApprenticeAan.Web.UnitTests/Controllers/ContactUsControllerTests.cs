06c93c349cdf0a721f5b00e7b6fca1f7b1744cd8
MarkFCain <mark.cain@digital.education.gov.uk>
2023-12-12 16:19:35 +0000

    CSP-987 controller method coverage added

---
+++ src/SFA.DAS.ApprenticeAan.Web.UnitTests/Controllers/ContactUsControllerTests.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.ApprenticeAan.Web.Controllers;
+
+namespace SFA.DAS.ApprenticeAan.Web.UnitTests.Controllers;
+public class ContactUsControllerTests
+{
+    [Test]
+    public void WhenGettingContactUs_ReturnsViewResult()
+    {
+        ContactUsController sut = new();
+
+        var result = sut.Index() as ViewResult;
+        result.Should().NotBeNull();
+    }
+}
