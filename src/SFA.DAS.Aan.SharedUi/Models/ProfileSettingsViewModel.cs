cde3dbb60acdf415a37f5e08d9784f5b5165eaa7
ManishAgarwalDfE <139247114+ManishAgarwalDfE@users.noreply.github.com>
2023-12-13 11:39:40 +0000

    CSP-855 Added NetworkHubLink property in ProfileSettingsViewModel

--- src/SFA.DAS.Aan.SharedUi/Models/ProfileSettingsViewModel.cs
+++ src/SFA.DAS.Aan.SharedUi/Models/ProfileSettingsViewModel.cs
@@ -1,6 +1,7 @@
 ﻿namespace SFA.DAS.Aan.SharedUi.Models;
-public class ProfileSettingsViewModel
+public class ProfileSettingsViewModel : INetworkHubLink
 {
     public string YourAmbassadorProfileUrl { get; set; } = "#";
     public string LeaveTheNetworkUrl { get; set; } = "#";
+    public string? NetworkHubLink { get; set; }
 }
