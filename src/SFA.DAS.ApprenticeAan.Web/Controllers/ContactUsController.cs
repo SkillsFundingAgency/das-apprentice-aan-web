86b71994137fac4cc72560a1109c94aaf5ce7cef
MarkFCain <mark.cain@digital.education.gov.uk>
2023-12-13 13:14:52 +0000

    CSP-987 adding config for contact us emails, and extending coverage

--- src/SFA.DAS.ApprenticeAan.Web/Controllers/ContactUsController.cs
+++ src/SFA.DAS.ApprenticeAan.Web/Controllers/ContactUsController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using SFA.DAS.Aan.SharedUi.Infrastructure;
+using SFA.DAS.ApprenticeAan.Web.Configuration;
+using SFA.DAS.ApprenticeAan.Web.Models;
 
 namespace SFA.DAS.ApprenticeAan.Web.Controllers;
 
@@ -8,10 +10,32 @@ namespace SFA.DAS.ApprenticeAan.Web.Controllers;
 [Route("contact-us", Name = SharedRouteNames.ContactUs)]
 public class ContactUsController : Controller
 {
+    private readonly ApplicationConfiguration _applicationConfiguration;
+
+
+    public ContactUsController(ApplicationConfiguration applicationConfiguration)
+    {
+        _applicationConfiguration = applicationConfiguration;
+    }
 
     [HttpGet]
     public IActionResult Index()
     {
-        return View();
+        var contactUsEmails = _applicationConfiguration.ContactUsEmails;
+
+        var viewModel = new ContactUsViewModel
+        {
+            EastMidlandsEmailAddress = contactUsEmails.EastMidlands,
+            EastOfEnglandEmailAddress = contactUsEmails.EastOfEngland,
+            LondonEmailAddress = contactUsEmails.London,
+            NorthEastEmailAddress = contactUsEmails.NorthEast,
+            NorthWestEmailAddress = contactUsEmails.NorthWest,
+            SouthEastEmailAddress = contactUsEmails.SouthEast,
+            SouthWestEmailAddress = contactUsEmails.SouthWest,
+            WestMidlandsEmailAddress = contactUsEmails.WestMidlands,
+            YorkshireAndTheHumberEmailAddress = contactUsEmails.YorkshireAndTheHumber
+        };
+
+        return View(viewModel);
     }
 }
