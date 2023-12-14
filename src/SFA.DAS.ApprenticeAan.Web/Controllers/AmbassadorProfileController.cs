9b5fe34c0ca2dae84be1ffaa7c41a39d31e9672a
ManishAgarwalDfE <139247114+ManishAgarwalDfE@users.noreply.github.com>
2023-12-14 10:15:27 +0000

    CSP-856 Fixed issue in url for member profile

--- src/SFA.DAS.ApprenticeAan.Web/Controllers/AmbassadorProfileController.cs
+++ src/SFA.DAS.ApprenticeAan.Web/Controllers/AmbassadorProfileController.cs
@@ -26,7 +26,7 @@ public class AmbassadorProfileController : Controller
         var profiles = _apiClient.GetProfilesByUserType(MemberUserType.Apprentice.ToString(), cancellationToken);
         var memberProfiles = _apiClient.GetMemberProfile(User.GetAanMemberId(), User.GetAanMemberId(), false, cancellationToken);
         await Task.WhenAll(profiles, memberProfiles);
-        var personalDetails = new PersonalDetailsModel(memberProfiles.Result.FullName, memberProfiles.Result.RegionName, memberProfiles.Result.UserType, Url.RouteUrl(SharedRouteNames.EditPersonalInformation)!, Url.RouteUrl(SharedRouteNames.EditAreaOfInterest)!, Url.RouteUrl(SharedRouteNames.EditContactDetail)!, Url.RouteUrl(SharedRouteNames.MemberProfile)!);
+        var personalDetails = new PersonalDetailsModel(memberProfiles.Result.FullName, memberProfiles.Result.RegionName, memberProfiles.Result.UserType, Url.RouteUrl(SharedRouteNames.EditPersonalInformation)!, Url.RouteUrl(SharedRouteNames.EditAreaOfInterest)!, Url.RouteUrl(SharedRouteNames.EditContactDetail)!, Url.RouteUrl(SharedRouteNames.MemberProfile, new { id = User.GetAanMemberId() })!);
         var apprenticeshipDetails = memberProfiles.Result.Apprenticeship != null ? new ApprenticeshipDetailsModel(memberProfiles.Result.Apprenticeship!.Sector, memberProfiles.Result.Apprenticeship!.Programme, memberProfiles.Result.Apprenticeship!.Level) : null;
         AmbassadorProfileViewModel model = new(personalDetails, memberProfiles.Result.Email, memberProfiles.Result.Profiles, memberProfiles.Result.Preferences, apprenticeshipDetails, profiles.Result.Profiles, Url);
         ViewBag.YourAmbassadorProfileSuccessMessage = TempData[TempDataKeys.YourAmbassadorProfileSuccessMessage];
