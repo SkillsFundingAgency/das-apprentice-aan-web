a2d6867931fc1fc1d377da063313d1ff34022e74
ManishAgarwalDfE <139247114+ManishAgarwalDfE@users.noreply.github.com>
2024-01-11 16:58:42 +0000

    CSP-1200 Added length restriction for LinkedIn url (#108)
    * CSP-1200 Added length restriction for LinkedIn url
    
    * CSP-1200 Updated System.IdentityModel.Tokens.JWT package
    
    * CSP-1200 Updated System.IdentityModel.Tokens.JWT package
    
    * CSP-1200 Updated System.IdentityModel.Tokens.JWT package
    
    * CSP-1200 Formatted the SubmitContactDetailModelValidator
    
    * CAP-1200 Allowed dash in linked in link

--- src/SFA.DAS.ApprenticeAan.Web/Validators/EditContactDetail/SubmitContactDetailModelValidator.cs
+++ src/SFA.DAS.ApprenticeAan.Web/Validators/EditContactDetail/SubmitContactDetailModelValidator.cs
@@ -1,14 +1,24 @@
 ﻿using FluentValidation;
 using SFA.DAS.Aan.SharedUi.Models.EditContactDetail;
+using static SFA.DAS.ApprenticeAan.Application.Constants.RegularExpressions;
 
 namespace SFA.DAS.ApprenticeAan.Web.Validators.EditContactDetail;
 
 public class SubmitContactDetailModelValidator : AbstractValidator<SubmitContactDetailModel>
 {
     public const string ShowLinkedinUrlValidationMessage = "You cannot select display check box without a value";
+    public const string LinkedinUrlLengthValidationMessage = "Your custom URL must contain 3-100 letters or numbers";
+    public const string LinkedinUrlPatternValidationMessage = "Your custom URL must not use spaces, symbols or special characters";
     public SubmitContactDetailModelValidator()
     {
         RuleLevelCascadeMode = CascadeMode.Stop;
+        RuleFor(x => x.LinkedinUrl).NotNull()
+            .WithMessage(LinkedinUrlLengthValidationMessage)
+            .Length(3, 100)
+            .WithMessage(LinkedinUrlLengthValidationMessage)
+            .Matches(AlphaNumericCharactersRegex)
+            .WithMessage(LinkedinUrlPatternValidationMessage);
+
         RuleFor(x => x.ShowLinkedinUrl)
             .Must((contactdetail, showlinkedinurl) => !showlinkedinurl)
             .When(x => string.IsNullOrEmpty(x.LinkedinUrl))
