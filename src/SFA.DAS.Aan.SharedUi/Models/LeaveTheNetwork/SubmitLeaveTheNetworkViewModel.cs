56b1bdf157fc945c5e2fa1e39385c3be8428afa4
MarkFCain <mark.cain@digital.education.gov.uk>
2024-01-11 15:04:37 +0000

    CSP-711 apprentice leaving the network

--- src/SFA.DAS.Aan.SharedUi/Models/LeaveTheNetwork/SubmitLeaveTheNetworkViewModel.cs
+++ src/SFA.DAS.Aan.SharedUi/Models/LeaveTheNetwork/SubmitLeaveTheNetworkViewModel.cs
@@ -4,6 +4,9 @@ public class SubmitLeaveTheNetworkViewModel
     public List<LeavingReasonModel>? LeavingReasons { get; set; }
     public List<LeavingReasonModel>? LeavingExperience { get; set; }
     public List<LeavingReasonModel>? LeavingBenefits { get; set; }
+    public string LeavingReasonsTitle { get; set; } = null!;
+    public string LeavingExperienceTitle { get; set; } = null!;
+    public string LeavingBenefitsTitle { get; set; } = null!;
 
     public int SelectedLeavingExperienceRating { get; set; } = 0;
 }
