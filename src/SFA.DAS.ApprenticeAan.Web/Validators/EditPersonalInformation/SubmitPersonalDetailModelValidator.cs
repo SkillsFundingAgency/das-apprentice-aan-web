f5042c8d01a99d82ba90d1714d1e9fd0d18212fc
MarkFCain <mark.cain@digital.education.gov.uk>
2024-04-03 15:31:18 +0100

    CSP-1285 added validator for biography

--- src/SFA.DAS.ApprenticeAan.Web/Validators/EditPersonalInformation/SubmitPersonalDetailModelValidator.cs
+++ src/SFA.DAS.ApprenticeAan.Web/Validators/EditPersonalInformation/SubmitPersonalDetailModelValidator.cs
@@ -8,10 +8,27 @@ public class SubmitPersonalDetailModelValidator : AbstractValidator<SubmitPerson
     public const string BiographyValidationMessage = "Your biography must be 500 characters or less";
     public const string JobTitleValidationMessage = "Your job title must be 200 characters or less";
     public const string JobTitleRequiredValidationMessage = "Your job title must not be empty";
+    public const string JobTitlePatternValidationMessage = "Your job title must be alphanumeric";
+    public const string BiographyHasExcludedCharacter = "Your biography must not include any special characters: @, #, $, ^, =, +, \\, /, <, >, %";
+    public const string ExcludedCharactersRegex = @"^[^@#$^=+\\\/<>%]*$";
+    public const string JobTitlePatternRegex = "^[a-zA-Z0-9 ]*$";
+
     public SubmitPersonalDetailModelValidator()
     {
         RuleLevelCascadeMode = CascadeMode.Stop;
-        RuleFor(x => x.Biography).Length(0, 500).WithMessage(BiographyValidationMessage);
-        RuleFor(x => x.JobTitle).NotNull().WithMessage(JobTitleRequiredValidationMessage).Length(0, 200).WithMessage(JobTitleValidationMessage);
+        RuleFor(x => x.Biography).Length(0, 500)
+            .WithMessage(BiographyValidationMessage)
+            .Matches(ExcludedCharactersRegex)
+            .WithMessage(BiographyHasExcludedCharacter);
+
+        RuleFor(x => x.JobTitle)
+            .Cascade(CascadeMode.Stop)
+            .NotNull()
+            .WithMessage(JobTitleRequiredValidationMessage)
+            .Matches(JobTitlePatternRegex)
+            .WithMessage(JobTitlePatternValidationMessage)
+            .Length(0, 200)
+            .WithMessage(JobTitleValidationMessage);
+
     }
 }
