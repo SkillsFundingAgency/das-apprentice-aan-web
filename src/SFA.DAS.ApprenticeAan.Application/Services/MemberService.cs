b76c9f8ffa9babac9cc40b3a370a55cf4107326e
shaun-downey-education <148352239+shaun-downey-education@users.noreply.github.com>
2024-04-22 11:31:19 +0100

    CSP-1309: Apprentice Details Sync When Logging Into AAN (#128)
    * - CSP-1309: Update details when logging onto AAN for the first time via the RequiresExistingMemberAttribute.
    
    * CSP-1309: Update claims helper to pull in last or default claim by claim identifier.
    
    * CSP-1309: Additional unit test coverage for MemberService.
    
    * CSP-1309: SonarCloud code suggestions - Update unit tests to utilize new nunit expressions.
    
    * CSP-1309: SonarCloud code suggestion - use collection specific exists method.
    
    * - CSP-1309: Update details when logging onto AAN for the first time via the RequiresExistingMemberAttribute.
    
    * CSP-1309: Update claims helper to pull in last or default claim by claim identifier.
    
    * CSP-1309: Additional unit test coverage for MemberService.
    
    * CSP-1309: SonarCloud code suggestions - Update unit tests to utilize new nunit expressions.
    
    * CSP-1309: SonarCloud code suggestion - use collection specific exists method.

---
+++ src/SFA.DAS.ApprenticeAan.Application/Services/MemberService.cs
@@ -0,0 +1,28 @@
+﻿using SFA.DAS.ApprenticeAan.Domain.Interfaces;
+using SFA.DAS.ApprenticeAan.Domain.OuterApi.Requests;
+
+namespace SFA.DAS.ApprenticeAan.Application.Services
+{
+    public class MemberService : IMemberService
+    {
+        private readonly IOuterApiClient _client;
+
+        public MemberService(IOuterApiClient client)
+        {
+            _client = client;
+        }
+
+        public async Task UpdateMemberDetails(Guid apprenticeId, string firstName, string lastName, CancellationToken cancellationToken)
+        {
+            UpdateMemberProfileAndPreferencesRequest updateMemberProfileRequest = new()
+            {
+                PatchMemberRequest = new PatchMemberRequest()
+                {
+                    FirstName = firstName,
+                    LastName = lastName
+                }
+            };
+            await _client.UpdateMemberProfileAndPreferences(apprenticeId, updateMemberProfileRequest, cancellationToken);
+        }
+    }
+}
