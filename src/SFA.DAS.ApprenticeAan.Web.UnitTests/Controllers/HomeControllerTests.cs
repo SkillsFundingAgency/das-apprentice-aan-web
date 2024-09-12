47bb36db6ea7c0a07f992a7eefd5077f44b3a2f6
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2024-09-12 07:46:23 +0100

    FAI-1314 - Add updating of user email if claim value different

--- src/SFA.DAS.ApprenticeAan.Web.UnitTests/Controllers/HomeControllerTests.cs
+++ src/SFA.DAS.ApprenticeAan.Web.UnitTests/Controllers/HomeControllerTests.cs
@@ -1,18 +1,26 @@
-﻿using FluentAssertions;
+﻿using System.Security.Claims;
+using AutoFixture.NUnit3;
+using FluentAssertions;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using SFA.DAS.Aan.SharedUi.Constants;
 using SFA.DAS.Aan.SharedUi.Infrastructure;
 using SFA.DAS.ApprenticeAan.Domain.Interfaces;
+using SFA.DAS.ApprenticeAan.Web.Configuration;
 using SFA.DAS.ApprenticeAan.Web.Controllers;
 using SFA.DAS.ApprenticeAan.Web.Infrastructure;
+using SFA.DAS.ApprenticePortal.Authentication;
+using SFA.DAS.GovUK.Auth.Services;
+using SFA.DAS.Testing.AutoFixture;
 
 namespace SFA.DAS.ApprenticeAan.Web.UnitTests.Controllers;
 
 public class HomeControllerTests
 {
     [Test]
-    public void Index_ApprenticeIsMember_RedirectsToNetworkHub()
+    public async Task Index_ApprenticeIsMember_RedirectsToNetworkHub()
     {
         var memberId = Guid.NewGuid().ToString();
         var sessionServiceMock = new Mock<ISessionService>();
@@ -20,34 +28,46 @@ public class HomeControllerTests
         sessionServiceMock.Setup(x => x.Get(Constants.SessionKeys.Member.Status)).Returns(MemberStatus.Live.ToString());
 
 
-        var sut = new HomeController(sessionServiceMock.Object);
+        var sut = new HomeController(
+            sessionServiceMock.Object,
+            Mock.Of<ApplicationConfiguration>(), 
+            Mock.Of<IOidcService>(), 
+            Mock.Of<IApprenticeAccountProvider>());
 
-        var result = sut.Index();
+        var result = await sut.Index();
 
         result.As<RedirectToRouteResult>().RouteName.Should().Be(RouteNames.NetworkHub);
     }
 
     [Test]
-    public void Index_ApprenticeIsMember_RedirectsToOnboardingBeforeYouStart()
+    public async Task Index_ApprenticeIsMember_RedirectsToOnboardingBeforeYouStart()
     {
-        var sut = new HomeController(Mock.Of<ISessionService>());
-        var result = sut.Index();
+        var sut = new HomeController(
+            Mock.Of<ISessionService>(), 
+            Mock.Of<ApplicationConfiguration>(), 
+            Mock.Of<IOidcService>(), 
+            Mock.Of<IApprenticeAccountProvider>()
+            );
+        var result = await sut.Index();
         result.As<RedirectToRouteResult>().RouteName.Should().Be(RouteNames.Onboarding.BeforeYouStart);
     }
 
     [TestCase(MemberStatus.Withdrawn, SharedRouteNames.RejoinTheNetwork)]
     [TestCase(MemberStatus.Deleted, SharedRouteNames.RejoinTheNetwork)]
     [TestCase(MemberStatus.Removed, SharedRouteNames.RemovedShutter)]
-    public void Index_MemberIsNotLive_RedirectsToRejoinTheNetwork(MemberStatus memberStatus, string routeName)
+    public async Task Index_MemberIsNotLive_RedirectsToRejoinTheNetwork(MemberStatus memberStatus, string routeName)
     {
         var memberId = Guid.NewGuid().ToString();
         var sessionServiceMock = new Mock<ISessionService>();
         sessionServiceMock.Setup(x => x.Get(Constants.SessionKeys.Member.MemberId)).Returns(memberId.ToString());
         sessionServiceMock.Setup(x => x.Get(Constants.SessionKeys.Member.Status)).Returns(memberStatus.ToString());
 
-        var sut = new HomeController(sessionServiceMock.Object);
+        var sut = new HomeController(sessionServiceMock.Object,
+            Mock.Of<ApplicationConfiguration>(), 
+            Mock.Of<IOidcService>(), 
+            Mock.Of<IApprenticeAccountProvider>());
 
-        var result = sut.Index();
+        var result = await sut.Index();
 
         result.As<RedirectToRouteResult>().RouteName.Should().Be(routeName);
     }
--- src/SFA.DAS.ApprenticeAan.Web/Controllers/HomeController.cs
+++ src/SFA.DAS.ApprenticeAan.Web/Controllers/HomeController.cs
@@ -1,10 +1,15 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using SFA.DAS.Aan.SharedUi.Constants;
 using SFA.DAS.Aan.SharedUi.Infrastructure;
 using SFA.DAS.ApprenticeAan.Domain.Interfaces;
+using SFA.DAS.ApprenticeAan.Web.Configuration;
 using SFA.DAS.ApprenticeAan.Web.Extensions;
 using SFA.DAS.ApprenticeAan.Web.Infrastructure;
+using SFA.DAS.ApprenticePortal.Authentication;
+using SFA.DAS.GovUK.Auth.Services;
 
 namespace SFA.DAS.ApprenticeAan.Web.Controllers;
 
@@ -14,14 +19,31 @@ namespace SFA.DAS.ApprenticeAan.Web.Controllers;
 public class HomeController : Controller
 {
     private readonly ISessionService _sessionService;
+    private readonly IOidcService _oidcService;
+    private readonly ApplicationConfiguration _configuration;
+    private readonly IApprenticeAccountProvider _apprenticeAccountProvider;
 
-    public HomeController(ISessionService sessionService)
+    public HomeController(ISessionService sessionService, ApplicationConfiguration configuration, IOidcService oidcService, IApprenticeAccountProvider apprenticeAccountProvider)
     {
         _sessionService = sessionService;
+        _oidcService = oidcService;
+        _configuration = configuration;
+        _apprenticeAccountProvider = apprenticeAccountProvider;
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
+        if (_configuration is { UseGovSignIn: true, UseStubAuth: false })
+        {
+            var token = await HttpContext.GetTokenAsync("access_token");
+            var govUkUser = await _oidcService.GetAccountDetails(token);
+            var loggedInUserEmail = User.EmailAddressClaim()!.Value;
+            if (!govUkUser.Email.Equals(loggedInUserEmail, StringComparison.CurrentCultureIgnoreCase))
+            {
+                await _apprenticeAccountProvider.PutApprenticeAccount(govUkUser.Email, govUkUser.Sub);
+            }
+        }
+        
         if (_sessionService.GetMemberId() == Guid.Empty)
         {
             return new RedirectToRouteResult(RouteNames.Onboarding.BeforeYouStart, null);
