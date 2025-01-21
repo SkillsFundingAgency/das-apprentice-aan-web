b149e8c95e7d784a8fcfacbaf1cf8e882d4ad2ae
David Rimar <david.rimar@digital.education.gov.uk>
2025-01-21 16:32:02 +0000

    EC-804 ReceiveNotifications page

---
+++ src/SFA.DAS.ApprenticeAan.Web/Validators/Onboarding/ReceiveNotificationsSubmitModelValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using SFA.DAS.ApprenticeAan.Web.Models.Shared;
+
+namespace SFA.DAS.ApprenticeAan.Web.Validators.Onboarding;
+
+public class ReceiveNotificationsSubmitModelValidator : AbstractValidator<ReceiveNotificationsSubmitModel>
+{
+    public const string NoSelectionErrorMessage = "Select if you want to receive a monthly email about upcoming events";
+
+    public ReceiveNotificationsSubmitModelValidator()
+    {
+        RuleFor(m => m.ReceiveNotifications)
+            .NotNull()
+            .WithMessage(NoSelectionErrorMessage);
+    }
+}
