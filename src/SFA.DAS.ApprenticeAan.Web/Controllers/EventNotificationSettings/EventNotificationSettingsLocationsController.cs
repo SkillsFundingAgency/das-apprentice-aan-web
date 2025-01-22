20f856331de602b170fec1e23eb02ea689c32d93
Chris Foster <chrisfoster186@googlemail.com>
2025-01-22 11:42:58 +0000

    WIP

---
+++ src/SFA.DAS.ApprenticeAan.Web/Controllers/EventNotificationSettings/EventNotificationSettingsLocationsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.ApprenticeAan.Domain.Interfaces;
+using SFA.DAS.ApprenticeAan.Web.Infrastructure;
+using SFA.DAS.ApprenticeAan.Web.Models.EventNotificationSettings;
+using SFA.DAS.ApprenticeAan.Web.Orchestrators;
+
+namespace SFA.DAS.ApprenticeAan.Web.Controllers.EventNotificationSettings
+{
+    [Authorize]
+    [Route("event-notification-settings/locations", Name = RouteNames.EventNotificationSettings.SettingsLocations)]
+    public class EventNotificationSettingsLocationsController(
+        IEventNotificationSettingsOrchestrator orchestrator,
+        ISessionService sessionService)
+        : Controller
+    {
+        public const string ViewPath = "~/Views/EventNotificationSettings/Locations.cshtml";
+
+        [HttpGet]
+        public async Task<IActionResult> Index(CancellationToken cancellationToken)
+        {
+            var sessionModel = sessionService.Get<NotificationSettingsSessionModel>();
+
+            return View(ViewPath);
+        }
+    }
+}
