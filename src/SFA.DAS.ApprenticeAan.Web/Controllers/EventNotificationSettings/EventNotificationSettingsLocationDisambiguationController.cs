41cfb58a24c5e0b08393974ba23a023243dface8
Chris Foster <chrisfoster186@googlemail.com>
2025-01-24 11:40:24 +0000

    WIP

--- src/SFA.DAS.ApprenticeAan.Web/Controllers/EventNotificationSettings/EventNotificationSettingsLocationDisambiguationController.cs
+++ src/SFA.DAS.ApprenticeAan.Web/Controllers/EventNotificationSettings/EventNotificationSettingsLocationDisambiguationController.cs
@@ -3,25 +3,48 @@ using Microsoft.AspNetCore.Mvc;
 using SFA.DAS.ApprenticeAan.Domain.Interfaces;
 using SFA.DAS.ApprenticeAan.Web.Infrastructure;
 using SFA.DAS.ApprenticeAan.Web.Models.EventNotificationSettings;
+using SFA.DAS.ApprenticeAan.Web.Orchestrators.Shared;
+using SFA.DAS.Validation.Mvc.Filters;
 
 namespace SFA.DAS.ApprenticeAan.Web.Controllers.EventNotificationSettings
 {
     [Authorize]
     [Route("event-notification-settings/locations/disambiguation", Name = RouteNames.EventNotificationSettings.SettingsNotificationLocationDisambiguation)]
-    public class EventNotificationSettingsLocationDisambiguationController : Controller
+    public class EventNotificationSettingsLocationDisambiguationController(
+        ISessionService sessionService,
+        INotificationLocationDisambiguationOrchestrator orchestrator)
+        : Controller
     {
         public const string ViewPath = "~/Views/EventNotificationSettings/NotificationLocationDisambiguation.cshtml";
-        private readonly ISessionService _sessionService;
 
-        public EventNotificationSettingsLocationDisambiguationController(ISessionService sessionService)
+        [HttpGet]
+        [ValidateModelStateFilter]
+        public async Task<IActionResult> Get(int radius, string location)
         {
-            _sessionService = sessionService;
+            var model = await orchestrator.GetViewModel<NotificationLocationDisambiguationViewModel>(radius, location);
+
+            model.BackLink = Url.RouteUrl(@RouteNames.EventNotificationSettings.NotificationLocations)!;
+
+            return View(ViewPath, model);
         }
 
-        public IActionResult Index()
+        [HttpPost]
+        [ValidateModelStateFilter]
+        public async Task<IActionResult> Post(NotificationLocationDisambiguationSubmitModel submitModel, CancellationToken cancellationToken)
         {
-            var sessionModel = _sessionService.Get<NotificationSettingsSessionModel>();
-            return View(ViewPath);
+            var result = await orchestrator.ApplySubmitModel<NotificationSettingsSessionModel>(submitModel, ModelState);
+
+            var routeValues = new { submitModel.Radius, submitModel.Location };
+
+            switch (result)
+            {
+                case NotificationLocationDisambiguationOrchestrator.RedirectTarget.NextPage:
+                    return RedirectToRoute(RouteNames.EventNotificationSettings.NotificationLocations);
+                case NotificationLocationDisambiguationOrchestrator.RedirectTarget.Self:
+                    return RedirectToRoute(RouteNames.EventNotificationSettings.SettingsNotificationLocationDisambiguation, routeValues);
+                default:
+                    throw new InvalidOperationException("Unexpected redirect target from ApplySubmitModel");
+            }
         }
     }
 }
