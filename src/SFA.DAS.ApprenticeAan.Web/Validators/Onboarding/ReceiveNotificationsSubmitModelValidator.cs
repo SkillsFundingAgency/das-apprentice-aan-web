0513aff2a51f1ea018d4e0bdbf0d78460a68972f
David Rimar <david.rimar@digital.education.gov.uk>
2025-01-28 15:10:55 +0000

    EC-803 fix ref error

--- src/SFA.DAS.ApprenticeAan.Web/Validators/Onboarding/ReceiveNotificationsSubmitModelValidator.cs
+++ src/SFA.DAS.ApprenticeAan.Web/Validators/Onboarding/ReceiveNotificationsSubmitModelValidator.cs
@@ -4,7 +4,7 @@ using SFA.DAS.ApprenticeAan.Web.Models.Onboarding;
 
 namespace SFA.DAS.ApprenticeAan.Web.Validators.Onboarding;
 
-public class ReceiveNotificationsSubmitModelValidator : AbstractValidator<ReceiveNotificationsSubmitModel>
+public class ReceiveNotificationsSubmitModelValidator : AbstractValidator<Models.Shared.ReceiveNotificationsSubmitModel>
 {
     public const string NoSelectionErrorMessage = "Select if you want to receive a monthly email about upcoming events";
 
